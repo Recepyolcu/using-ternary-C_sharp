9cee0368da65b86eb5294640a87f36f17aec430c
Recep Yolcu <reeceep05@gmail.com>
2022-09-18 18:57:12 +0300

    ternary yapısı kullanılarak program yazıldı

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace if_else_ternary
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            string text = time <= 24 && time >= 20 ? "good night" : time <= 20 && time >= 16 ? "good evening" : "good afternoon";
+            Console.WriteLine(text); 
+            Console.ReadKey();
+        }
+    }
+}
